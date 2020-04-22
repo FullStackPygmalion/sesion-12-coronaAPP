bd66c1839edeab6de4c2de7ac6232cb6e47551c6
Juan Giovanni Restrepo <grestrepojimenez@gmail.com>
2020-04-22 18:49:12 -0500

    Add Resultados y grafica de coronavirus colombia

--- js/index.js
+++ js/index.js
@@ -9,6 +9,7 @@ fetch("https://www.datos.gov.co/resource/gt2j-8ykr.json?$limit=5000", {
     .then(myJson => {
         datosCoronavirus = myJson;
         mostrarDatos(myJson);
+        dibujarGraficas(myJson)
     })
     .catch(err => {
         console.log(err);
@@ -44,12 +45,89 @@ let calcularProbabilidad = function() {
 
     let inputs = document.querySelectorAll('#formularioProbabilidad input')
     Array.from(inputs).forEach((elm) => {
-        if(elm.type=='radio'){
-            if(elm.checked) {persona[elm.name] = elm.value}
+        if (elm.type == 'radio') {
+            if (elm.checked) { persona[elm.name] = elm.value }
         } else {
             persona[elm.name] = elm.value
         }
     })
 
+    let contagio;
+
+    let casosDepartamento = datosCoronavirus.filter(({ departamento }) => persona.departamento == departamento)
+
+    contagio = (casosDepartamento.length * 100 / datosCoronavirus.length)
+
+
+    switch (persona.salidas) {
+        case 'ninguna':
+            break;
+        case '1 a 3':
+            contagio *= 4
+            break;
+        case '4 a 10':
+            contagio *= 8
+            break;
+        case 'mas de 10':
+            contagio *= 16
+            break;
+        default:
+            break;
+    }
+
+    if (contagio < 0) contagio = 0
+    if (contagio > 100) contagio = 100
+    contagio = Math.floor(contagio)
+
+    let mensaje = `Señor ${persona.nombre} siga teniendo en cuentra las medidas propiestas por las organizaciones de salud `
+
+    let estadisticas = `<br><p>Los casos en su departamento son: <br><span class = "big-text-3">${casosDepartamento.length} <span></p>`
+
+    document.getElementById('probabilidad').innerText = contagio + '%'
+    document.getElementById('mensaje').innerText = mensaje
+    document.getElementById('estadisticas-resultados').innerHTML = estadisticas
+
 }
 
+
+let dibujarGraficas = function(datosCoronavirus) {
+
+    let fechasContagio = datosCoronavirus.map(({ fecha_diagnostico }) => fecha_diagnostico)
+
+    let aumentoXdia = []
+    let indice = 0,
+        dia = 0,
+        contagios = 0
+
+
+    fechasContagio.forEach(fecha => {
+        if (fechasContagio.indexOf(fecha) == indice) {
+            contagios++
+            aumentoXdia[dia] = contagios
+            dia++;
+        } else {
+            contagios++
+            aumentoXdia[dia] = contagios
+        }
+        indice++;
+    })
+
+    console.log(aumentoXdia)
+
+
+
+    new Chartist.Line('.ct-chart', {
+        labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
+        series: [
+            aumentoXdia
+
+        ]
+    }, {
+        fullWidth: true,
+        chartPadding: {
+            right: 40
+        }
+    });
+
+
+}
